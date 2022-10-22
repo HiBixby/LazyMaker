6ae3517691098f43e7a816549cd114c35de25cfd
HiBixby <201910789@sangmyung.kr>
2022-10-22 19:20:16 +0900

    🤡 Add mock basic components

---
+++ src/components/Main.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen flex flex-col justify-center items-center">
+    <div class="text-3xl font-bold">나의 유형 테스트</div>
+    <img class="rounded-full" src="../assets/skyblue.png"/>
+    <router-link class="text-3xl" to="/questions">시작하기</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+  props: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.router-link-exact-active {
+  color: red;
+}
+</style>
---
+++ src/components/Questions.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-3xl font-bold">질문</div>
+</template>
+
+<script>
+export default {
+  name: "QuestionsPage",
+  props: {
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
