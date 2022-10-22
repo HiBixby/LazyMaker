58b4647e852508338de93505caf6ba36563b5925
HiBixby <201910789@sangmyung.kr>
2022-10-22 20:31:26 +0900

    💄 Change style of main page a bit

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="h-screen flex flex-col justify-center items-center">
-    <div class="text-3xl font-bold">나의 유형 테스트</div>
-    <img class="rounded-full" src="../assets/skyblue.png"/>
-    <router-link class="text-3xl" to="/questions">시작하기</router-link>
+    <div class="">ㅁㅁ로 알아보는 유형 테스트</div>
+    <div class="text-3xl font-bold">나는 어떤 유형일까?</div>
+    <img class="rounded-full my-5 w-48" src="../assets/skyblue.png"/>
+    <router-link class="text-xl rounded-md bg-blue-300 p-3" to="/questions">시작하기</router-link>
   </div>
 </template>
 
