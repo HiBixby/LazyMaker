7958d50738d733369ad873c1fedcd2bce7af05c9
HiBixby <201910789@sangmyung.kr>
2022-10-27 04:34:35 +0900

    🚧 Apply a new design

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="min-h-full flex flex-col justify-center items-center">
-    <div class="">ㅁㅁ로 알아보는 유형 테스트</div>
-    <div class="text-3xl font-bold">나는 어떤 유형일까?</div>
-    <img class="rounded-full my-24 w-48" src="../assets/skyblue.png" />
+    <div class="text-[1.688rem] leading-[1.19] tracking-[-0.68px] text-black font-bold">나는 어떨 때<br/>꾸벅꾸벅 졸까..?</div>
+    <img class="w-[38.8%]" src="../assets/logo.svg" />
+    <div class="font-thin text-xs">흠냐흠냐...잠이온다..</div>
     <router-link
-      class="text-xl rounded-md bg-blue-300 py-3 w-48"
+      class="text-white text-[.813rem] font-bold rounded-md bg-custom-blue py-3 w-[44.6%]"
       to="/questions"
-      >시작하기</router-link
+      >내 지루함 알아보기</router-link
     >
   </div>
 </template>
@@ -20,4 +20,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.btn-start{
+  color:white;
+}
 </style>
