a0174a23699d27a04694690661558e131eafce9d
HiBixby <201910789@sangmyung.kr>
2022-10-27 09:41:29 +0900

    🚧 Apply UI 90%

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="min-h-full flex flex-col justify-center items-center">
-    <div class="text-[1.688rem] leading-[1.19] tracking-[-0.68px] text-black font-bold">나는 어떨 때<br/>꾸벅꾸벅 졸까..?</div>
-    <img class="w-[38.8%]" src="../assets/logo.svg" />
-    <div class="font-thin text-xs">흠냐흠냐...잠이온다..</div>
+    <div
+      class="title text-[1.688rem] leading-[1.19] tracking-[-0.68px] text-black font-bold"
+    >
+      나는 어떨 때<br />꾸벅꾸벅 졸까..?
+    </div>
+    <img class="img w-[38.8%]" src="../assets/logo.svg" />
+    <div class="sub font-thin text-xs">흠냐흠냐...잠이온다..</div>
     <router-link
-      class="text-white text-[.813rem] font-bold rounded-md bg-custom-blue py-3 w-[44.6%]"
+      class="btn text-white text-[0.813rem] font-bold rounded-md bg-custom-blue py-4 w-[44.6%]"
       to="/questions"
       >내 지루함 알아보기</router-link
     >
@@ -20,7 +24,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.btn-start{
-  color:white;
+.title {
+  margin: 0rem 0 1.934rem 0;
+}
+.img {
+  margin: 0 0 0.658rem 0;
+}
+.sub {
+  margin: 0 0 2.313rem 0;
+}
+.btn {
+  margin: 0;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.15;
+  letter-spacing: normal;
 }
 </style>
