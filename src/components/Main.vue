15ed0498e4d73e04e7f7c0e89caa571060cbadcd
HiBixby <201910789@sangmyung.kr>
2022-11-22 07:52:15 +0900

    ✏️ main 페이지 og태그 오타 수정

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -23,9 +23,9 @@ export default {
     useMeta({
       title:"Lazy maker - 메인",
       meta:[
-        {vmid:'og.title',name:'og.title',content:'Lazy maker'},
-        {vmid:'og.description',name:'og.description',content:'나는 어떤 유형의 수업을 들을까?'},
-        {vmid:'og.image',name:'og.image',content:'https://raw.githubusercontent.com/HiBixby/LazyMaker_Vue/2f530883610196c0889f266b6ec970bb6f8ea877/src/assets/logo.svg'},
+        {vmid:'og:title',property:'og:title',content:'Lazy maker'},
+        {vmid:'og:description',property:'og:description',content:'나는 어떤 유형의 수업을 들을까?'},
+        {vmid:'og:image',property:'og:image',content:'https://raw.githubusercontent.com/HiBixby/LazyMaker_Vue/2f530883610196c0889f266b6ec970bb6f8ea877/src/assets/logo.svg'},
       ]
     })
   },
